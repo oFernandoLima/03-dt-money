bf8d4b911bfb56278afb48b74adb78b74fbe8692
Fernando Lima <eufernandoprogramador@gmail.com>
2024-01-31 22:50:06 -0300

    feat: SearchForm component

--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -11,11 +11,11 @@ export const TransactionsTable = styled.table`
   width: 100%;
   border-collapse: separate;
   border-spacing: 0 0.5rem;
-  /* margin-top: 1.5rem; */
+  margin-top: 1.5rem;
 
   td {
     padding: 1.25rem 2rem;
-    background: ${({ theme }) => theme['gray-700']};
+    background: ${(props) => props.theme['gray-700']};
 
     &:first-child {
       border-top-left-radius: 6px;
